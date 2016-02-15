07b66c91f4c016513dba352f6f89e78a8d480237
Joe Kent <thedeadlybutter@gmail.com>
2016-02-15 16:56:38 -0500

    Adding support infastructure

--- app.js
+++ app.js
@@ -1,11 +1,21 @@
 var express = require('express');
 var app = express();
-var server;
+var bodyParser = require('body-parser');
+var session = require('client-sessions');
+
+app.use(bodyParser.json());
+
+app.use(session({
+  cookieName: 'wallet',
+  secret: process.env.SESSION_KEY,
+  duration: 100 * 24 * 60 * 60 * 1000, // 100 days
+  activeDuration: 100 * 24 * 60 * 60 * 1000,
+}));
 
 app.get('/', function(req, res){
   res.send('Hello World');
 });
 
-server = app.listen(3000, function() {
+var server = app.listen(process.env.PORT, function() {
  console.log('Listening on port %d', server.address().port);
 });
